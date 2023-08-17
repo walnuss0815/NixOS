00ea4a33b2e7ebc8bdbbb1756b44b5ae1ed0d5bd
Alexander Weidemann <walnuss0815@gmail.com>
2023-08-17 21:19:47 +0200

    fixup! Initial commit

--- modules/gnome.nix
+++ modules/gnome.nix
@@ -5,6 +5,10 @@
   ...
 }:
 {
+  services.xserver.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
   environment.gnome.excludePackages = (with pkgs; [
     gnome-tour
   ]) ++ (with pkgs.gnome; [
