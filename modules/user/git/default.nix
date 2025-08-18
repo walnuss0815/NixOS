709132c6ca5a79c28c8f6cc3a23745d4c84e15ae
Alexander Weidemann <walnuss0815@gmail.com>
2025-08-18 10:41:41 +0200

    chore(flake): update lock file

--- modules/user/git/default.nix
+++ modules/user/git/default.nix
@@ -1,38 +1,40 @@
 { pkgs, config, ... }: {
   programs.git = {
     enable = true;
-    userName = "Alexander Weidemann";
-    userEmail = "walnuss0815@gmail.com";
-    lfs.enable = true;
-    aliases = {
-      co = "checkout";
-      br = "branch";
-      ci = "commit";
-      st = "status";
-      unstage = "reset HEAD --";
-      up = "fetch --prune --all";
-      graph = "log --oneline --graph --decorate";
-      fpush = "push --force-with-lease";
-      mr =
-        "push --push-option=merge_request.create --push-option=merge_request.draft";
-      wip = "commit --message='WIP'";
-      track = "add --intent-to-add";
-      tow = "pull --recurse-submodules=on-demand";
-      irb = "rebase --interactive --autosquash";
-      repo = "remote --verbose";
-      fuck = "commit --fixup";
-      pfuck = "commit --patch --fixup";
-      fix = "commit --amend --no-edit";
-      pfix = "commit --amend --no-edit --patch";
-      hash = "rev-parse HEAD";
-      sub = "submodule";
-      edit = "commit --amend --only";
-      cf = "diff-tree --no-commit-id --name-only";
-      cfr = "cf -r";
-      asq = "rebase --autosquash";
-      cb = "rev-parse --abbrev-ref HEAD";
-    };
-    extraConfig = {
+    settings = {
+      user = {
+        name = "Alexander Weidemann";
+        email = "walnuss0815@gmail.com";
+      };
+      lfs.enable = true;
+      aliases = {
+        co = "checkout";
+        br = "branch";
+        ci = "commit";
+        st = "status";
+        unstage = "reset HEAD --";
+        up = "fetch --prune --all";
+        graph = "log --oneline --graph --decorate";
+        fpush = "push --force-with-lease";
+        mr =
+          "push --push-option=merge_request.create --push-option=merge_request.draft";
+        wip = "commit --message='WIP'";
+        track = "add --intent-to-add";
+        tow = "pull --recurse-submodules=on-demand";
+        irb = "rebase --interactive --autosquash";
+        repo = "remote --verbose";
+        fuck = "commit --fixup";
+        pfuck = "commit --patch --fixup";
+        fix = "commit --amend --no-edit";
+        pfix = "commit --amend --no-edit --patch";
+        hash = "rev-parse HEAD";
+        sub = "submodule";
+        edit = "commit --amend --only";
+        cf = "diff-tree --no-commit-id --name-only";
+        cfr = "cf -r";
+        asq = "rebase --autosquash";
+        cb = "rev-parse --abbrev-ref HEAD";
+      };
       core.eol = "lf";
       core.autocrlf = false;
       merge.ff = "only";
