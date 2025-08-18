8ac2cd4f7033eaf99c7a418f8961a076cc01a881
Alexander Weidemann <walnuss0815@gmail.com>
2025-08-18 10:31:53 +0200

    fix(gnome): fix cursor

--- modules/user/gnome/default.nix
+++ modules/user/gnome/default.nix
@@ -11,6 +11,7 @@
       "org/gnome/desktop/interface" = {
         color-scheme = "prefer-dark";
         enable-hot-corners = false;
+        cursor-theme = "Adwaita";
       };
       "org/gnome/desktop/wm/preferences" = {
         button-layout = "appmenu:minimize,maximize,close";
