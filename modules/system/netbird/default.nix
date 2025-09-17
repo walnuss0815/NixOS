9c2adfbeb2bc1335ceec83d0db169049e5c63440
Alexander Weidemann <walnuss0815@gmail.com>
2025-09-17 18:25:44 +0200

    feat(netbird): disable auto connect

--- modules/system/netbird/default.nix
+++ modules/system/netbird/default.nix
@@ -1,4 +1,13 @@
 { config, pkgs, ... }: {
-  services.netbird.enable = true; # for netbird service & CLI
-  environment.systemPackages = [ pkgs.netbird-ui ]; # for GUI
+  services.netbird = {
+    enable = true;
+    ui.enable = true;
+    clients = {
+      personal = {
+        name = "netbird";
+        port = 51820;
+        autoStart = false;
+      };
+    };
+  };
 }
