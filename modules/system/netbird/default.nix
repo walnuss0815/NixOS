2c080d600260b9a5d478de144e7010f3f0362861
Alexander Weidemann <walnuss0815@gmail.com>
2025-10-20 11:45:22 +0200

    feat(netbird): update netbird config

--- modules/system/netbird/default.nix
+++ modules/system/netbird/default.nix
@@ -1,12 +1,17 @@
 { config, pkgs, ... }: {
   services.netbird = {
-    enable = true;
-    ui.enable = true;
+    useRoutingFeatures = "client";
     clients = {
       personal = {
-        name = "netbird";
+        name = "personal";
         port = 51820;
         autoStart = false;
+        ui.enable = true;
+        # # Need to fix the issue:
+        # # Exactly one of users.users.alexander.isSystemUser and users.users.alexander.isNormalUser must be set.
+        # user = {
+        #   name = "alexander";
+        # };
       };
     };
   };
