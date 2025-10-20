5e8823a484926f97674c534f5a8193cca93e435e
Alexander Weidemann <walnuss0815@gmail.com>
2025-10-20 11:45:43 +0200

    feat(bambu-studio): add program

--- users/alexander/default.nix
+++ users/alexander/default.nix
@@ -128,6 +128,9 @@
     ghorg # Clone GitHub orgs and Gitlab groups
 
     firefox
+    (bambu-studio.overrideAttrs (oldAttrs: {
+      cmakeFlags = oldAttrs.cmakeFlags ++ [ "-DCMAKE_POLICY_VERSION_MINIMUM=3.5" ];
+    }))
   ];
 
   # This value determines the home Manager release that your
