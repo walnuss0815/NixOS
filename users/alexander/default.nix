bd10e07b2d28bf8dc5febd095952aa32b06a1a4e
Alexander Weidemann <walnuss0815@gmail.com>
2025-12-23 11:27:23 +0100

    fix(bambu): remove bambu

--- users/alexander/default.nix
+++ users/alexander/default.nix
@@ -131,9 +131,6 @@
     restic
 
     firefox
-    (bambu-studio.overrideAttrs (oldAttrs: {
-      cmakeFlags = oldAttrs.cmakeFlags ++ [ "-DCMAKE_POLICY_VERSION_MINIMUM=3.5" ];
-    }))
   ];
 
   # This value determines the home Manager release that your
