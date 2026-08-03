7359c5d6edde94f2ead85eabcbcbd6f68c8c8303
Alexander Weidemann <walnuss0815@gmail.com>
2026-08-03 09:32:11 +0200

    feat(gnome): add wi-di support

--- modules/system/gnome/default.nix
+++ modules/system/gnome/default.nix
@@ -18,6 +18,20 @@
     gnome-tour # Tour
   ];
 
+  environment.systemPackages = with pkgs; [
+    gnome-network-displays
+  ];
+
+  xdg.portal.enable = true;
+  xdg.portal.extraPortals = [
+    pkgs.xdg-desktop-portal-gnome
+  ];
+
+  # Firewall ports for Miracast/Wi-Fi Direct
+  networking.firewall.trustedInterfaces = [ "p2p-wl+" ];
+  networking.firewall.allowedTCPPorts = [ 7236 7250 ];
+  networking.firewall.allowedUDPPorts = [ 7236 5353 ];
+
   services.udev.packages = with pkgs; [ gnome-settings-daemon ];
 
   # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
